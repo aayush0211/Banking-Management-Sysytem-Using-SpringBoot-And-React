10b769ed0bd1f1a6b24b2bb8f1346f14f3f9ef18
aayush0211 <ayushfeb2000@gmail.com>
2024-02-09 16:12:37 +0530

    CardDao created

---
+++ src/main/java/com/bank/dao/CardDao.java
@@ -0,0 +1,9 @@
+package com.bank.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bank.entities.Card;
+
+public interface CardDao extends JpaRepository<Card, Long> {
+
+}
