14422dae0d48ad1293866cf27242daf3abd5b1c0
aayush0211 <ayushfeb2000@gmail.com>
2024-02-11 09:39:26 +0530

    changes in global exception handler

--- src/main/java/com/bank/globalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/bank/globalExceptionHandler/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.bank.custom.exception.BranchNotFoundException;
+import com.bank.custom.exception.LimitExceededException;
 import com.bank.response.ErrorResponse;
 
 @ControllerAdvice
@@ -13,17 +14,24 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleGlobalException(Exception ex) {
-        // Create a custom error response
+        
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(BranchNotFoundException.class)
     public ResponseEntity<Object> handleBranchNotFoundException(BranchNotFoundException ex) {
-        // Create a custom error response
+        
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
+    
+    @ExceptionHandler(LimitExceededException.class)
+    public ResponseEntity<Object> handleLimitExceededException(LimitExceededException ex) {
+        
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.REQUESTED_RANGE_NOT_SATISFIABLE, ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.REQUESTED_RANGE_NOT_SATISFIABLE);
+    }
 
-    // Other exception handlers as needed
+   
 }
