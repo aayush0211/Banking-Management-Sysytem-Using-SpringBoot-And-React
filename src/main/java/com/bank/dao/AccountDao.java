e64b41837bfbc6e2f5b2b9aced01f3adc71cdbc7
aayush0211 <ayushfeb2000@gmail.com>
2024-02-12 16:30:08 +0530

    changes in dao

--- src/main/java/com/bank/dao/AccountDao.java
+++ src/main/java/com/bank/dao/AccountDao.java
@@ -1,9 +1,13 @@
 package com.bank.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bank.entities.Account;
 
 public interface AccountDao extends JpaRepository<Account, Long> {
+
+ Optional<Account> findByEmail(String email);
 
 }
--- src/main/java/com/bank/dao/EmployeeDao.java
+++ src/main/java/com/bank/dao/EmployeeDao.java
@@ -1,9 +1,12 @@
 package com.bank.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 
 import com.bank.entities.Employee;
 
 public interface EmployeeDao extends JpaRepository<Employee, Long> {
-
+	 Optional<Employee> findByEmail(String email);
 }
